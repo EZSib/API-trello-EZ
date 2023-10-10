b20889defdbbb9758ad2fb89b1e2a6dade5627b3
EZsib <efimzigunov@gmail.com>
2023-10-10 17:50:36 +0600

    add controller, module, service

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import {Controller} from "@nestjs/common";
+import {Get} from "@nestjs/common";
+import {AppService} from "./app.service";
+
+@Controller('/api')
+export class AppController {
+
+    constructor(private appService: AppService) {}
+
+    @Get('/users')
+    getUsers() {
+            return this.appService.getUsers()
+        }
+
+}
