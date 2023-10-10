793bf046ef21a1e30d1e2d0ab9db29be43265526
EZsib <efimzigunov@gmail.com>
2023-10-10 20:57:23 +0600

    create users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,27 @@
 import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
+import {User} from "./users/users.models";
 
 @Module( {
-    controllers : [AppController],
-    providers: [AppService],
+    controllers : [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgress',
-            password: 'root',
-            database: 'nest-proj',
-            models: [],
+            host: process.env.POSTGRESS_HOST,
+            port: Number(process.env.POSTGRESS_PORT),
+            username: process.env.POSTGRESS_USER,
+            password: process.env.POSTGRESS_PASSWORD,
+            database: process.env.POSTGRESS_DB,
+            models: [User],
             autoLoadModels: true
         }),
+        UsersModule,
     ]
 })
 export class AppModule {}
---
+++ src/users/users.models.ts
@@ -0,0 +1,20 @@
+import { Model, Table, Column, DataType} from "sequelize-typescript";
+
+interface UserCreationAttrs {
+    email: string;
+    password: string;
+}
+@Table({tableName: 'users'})
+export class User extends Model <User, UserCreationAttrs> {
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @Column({type: DataType.INTEGER, unique: true, allowNull: false})
+    email: string;
+    @Column({type: DataType.INTEGER, allowNull: false})
+    password: string;
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+    @Column({type: DataType.INTEGER, allowNull: true})
+    banReason: string;
+}
